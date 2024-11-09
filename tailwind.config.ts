4b6d41d91bc180a12d75117bbc56716d9f9133fc
Zerol Acqua <hlc118@outlook.com>
2024-11-09 09:07:21 +0800

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,22 @@
 import type { Config } from "tailwindcss";
+import { colors } from './config.json';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      ...colors,
     },
+    extend: {},
+  },
+  variants: {
+    extend: {},
   },
   plugins: [],
 } satisfies Config;
